fe7397b7f8ee42de0b73470d686b05883662d882
HighError <hierrorua@gmail.com>
2025-04-14 11:50:27 +0300

    feat(lint-staged): updated lint-staged config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,7 @@
 export default {
-  "*.{ts,tsx}": () => "tsc --noEmit",
+  // Run tsc on staged files that have the following extensions: ts, tsx
+  "*.{ts,tsx}": () => "tsc-files --noEmit",
+  // Run Biome on staged files that have the following extensions: js, ts, jsx, tsx, json and jsonc
   "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
     "biome check --files-ignore-unknown=true", // Check formatting and lint
     "biome check --write --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
@@ -8,6 +10,7 @@ export default {
     "biome format --write --no-errors-on-unmatched", // Format
     "biome lint --write --no-errors-on-unmatched", // Lint and apply safe fixes
   ],
+  // Alternatively you can pass every files and ignore unknown extensions
   "*": [
     "biome check --no-errors-on-unmatched --files-ignore-unknown=true", // Check formatting and lint
   ],
